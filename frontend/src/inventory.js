9dfaefbb0173e8568411901ee4c0c7e13df168e2
isaev <alexeyisaev29032001@gmail.com>
2023-12-18 01:30:49 +0300

    inventory

--- frontend/src/inventory.js
+++ frontend/src/inventory.js
@@ -1,36 +1,56 @@
-import React, { useState } from "react";
-import { Table, Flex, Button, Modal, Form, Input, Typography, Select } from "antd";
+import React, { useEffect, useState } from "react";
+import { Input, Table } from "antd";
+import { request } from "./utils/request";
+
+const ActionColumn = ({ item }) => {
+  return (
+    <div style={{ display: "flex" }}>
+      <Input size="small" />
+
+      <div>
+        <button style={{width: 24}}>+</button>
+        <button style={{width: 24}}>-</button>
+      </div>
+    </div>
+  );
+};
 
 const cols = [
   {
     title: "Product",
-    dataIndex: "name",
-    key: "name",
-  },
-  {
-    title: "Unit",
-    dataIndex: "unit",
-    key: "unit",
-  },
-  {
-    title: "Image",
-    dataIndex: "image",
-    key: "image",
-    render: () => {
-      return <img src="" alt="" />;
-    },
+    dataIndex: "id",
+    key: "id",
   },
   {
     title: "Amount",
     dataIndex: "amount",
     key: "amount",
   },
+  {
+    title: "",
+    render: (item) => {
+      return <ActionColumn item={item} />;
+    },
+    width: 230,
+  },
 ];
 
 export const Inventory = () => {
+  const [items, setItems] = useState([]);
+
+  const refreshItems = async () => {
+    const res = await request("/api/inventory/getAllInventoryItems");
+
+    setItems(res);
+  };
+
+  useEffect(() => {
+    refreshItems();
+  }, []);
+
   return (
-    <div>
-      <Table dataSource={[{}]} columns={cols} />
+    <div style={{maxWidth: 800}}>
+      <Table dataSource={items} columns={cols} />
     </div>
   );
 };
